5d901922680405a005be3d1eb183aaeca490754c
akhmelev <akhmelev@gmail.com>
2024-03-19 20:57:38 +0300

    ADD play game but not found end game

--- src/main/java/com/javarush/khmelov/cmd/Play.java
+++ src/main/java/com/javarush/khmelov/cmd/Play.java
@@ -8,13 +8,12 @@ import com.javarush.khmelov.service.QuestionService;
 import com.javarush.khmelov.util.Go;
 import com.javarush.khmelov.util.Key;
 import com.javarush.khmelov.util.Parser;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
 import java.util.Optional;
 
+@SuppressWarnings("unused")
 public class Play implements Command {
 
     private final GameService gameService;
@@ -34,20 +33,20 @@ public class Play implements Command {
             Optional<com.javarush.khmelov.entity.Game> game = gameService.getGame(questId, userId);
             if (game.isPresent()) {
                 showOneQuestion(request, game.get());
-                Long id = game.get().getId();
-                return getJspPage() + "?id=" + id;
+                Long gameId = game.get().getId();
+                return "%s?questId=%d&id=%d".formatted(getJspPage(), questId, gameId);
             } else {
-                createError(request,"Нет незавершенной игры");
+                createError(request, "Нет незавершенной игры");
                 return Go.HOME;
             }
         } else {
-            createError(request,"Сначала нужно войти в аккаунт");
+            createError(request, "Сначала нужно войти в аккаунт");
             return Go.LOGIN;
         }
     }
 
     @Override
-    public String doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public String doPost(HttpServletRequest request, HttpServletResponse response) {
         Long gameId = Parser.getId(request);
         Long answerId = Parser.getId(request, Key.ANSWER);
         Optional<com.javarush.khmelov.entity.Game> game = gameService.checkAnswer(gameId, answerId);
@@ -57,7 +56,7 @@ public class Play implements Command {
                 createError(request, "Нужно выбрать какой-то ответ");
             }
             showOneQuestion(request, gameDto);
-            return Go.PLAY+"?id=" + gameId;
+            return "%s?questId=%d&id=%d".formatted(Go.PLAY, game.get().getQuestId(), gameId);
         } else {
             createError(request, "Нет такой игры");
             return Go.HOME;
